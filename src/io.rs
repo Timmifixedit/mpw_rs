72e9b0e0839e6f617b53f23de33ab15b5b69a2b5
Timmifixedit <tim@luchterhand.org>
2025-09-22 19:09:20 +0200

    added transfer data function

--- src/io.rs
+++ src/io.rs
@@ -1,8 +1,7 @@
 use std::fs;
 use std::fs::File;
-use std::io::{Read, Seek};
-
-
+use std::io::{BufReader, BufWriter, Write, Read, Seek};
+use secure_string::SecureVec;
 
 pub fn read_bytes(file: &mut fs::File, len: usize, offset: std::io::SeekFrom, msg: &str) -> Result<Vec<u8>, String> {
     if let Err(err) = file.seek(offset) {
@@ -32,4 +31,27 @@ pub fn read_all(file: &String, offset: std::io::SeekFrom) -> Result<Vec<u8>, Str
         Ok(_) => Ok(ret),
         Err(msg) => Err(format!("Failed reading file contents: {}", msg.to_string()))
     }
+}
+
+pub fn transfer_data<Source: Read, Dest: Write>(source: Source, dest: &mut Dest) -> Result<(Source, usize), String> {
+    let mut b_source = BufReader::new(source);
+    let mut b_dest = BufWriter::new(dest);
+
+    let mut num_read_total = 0usize;
+    let mut num_read: usize;
+    let mut buf = SecureVec::new(vec![0u8; b_source.capacity()]);
+    loop {
+        num_read = b_source.read(&mut buf.unsecure_mut()).unwrap_or_default();
+        num_read_total += num_read;
+        if num_read == 0 {
+            break;
+        }
+
+        if let Err(err) = b_dest.write_all(&buf.unsecure()[..num_read]) {
+            return Err(format!("failed to write {num_read} bytes to destination buffer: {}", err.to_string()));
+        }
+    }
+
+    b_dest.flush().map_err(|err| format!("failed to flush destination buffer: {}", err.to_string()))?;
+    Ok((b_source.into_inner(), num_read_total))
 }
