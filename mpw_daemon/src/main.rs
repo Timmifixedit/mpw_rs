db1fbd1289a3bd2ac25607cd6c994a1af9b54d45
Timmifixedit <tim@luchterhand.org>
2026-02-06 15:25:16 +0100

    set socket permissions

--- mpw_daemon/src/main.rs
+++ mpw_daemon/src/main.rs
@@ -3,6 +3,8 @@ mod request_handler;
 
 use clap::Parser;
 use mpw_core::vault::Vault;
+use std::fs::Permissions;
+use std::os::unix::fs::PermissionsExt;
 use std::os::unix::net::UnixListener;
 use std::path::PathBuf;
 use std::process::exit;
@@ -30,7 +32,7 @@ fn main() {
 
     println!("Successfully loaded vault in {}", args.vault_path.display());
     let vault = vault.unwrap();
-    let socket_path = "/home/tim/mpw_socket";
+    let socket_path = "/home/tim/.mpw_socket";
     let _ = std::fs::remove_file(socket_path); // Remove existing socket if it exists
     let listener = match UnixListener::bind(socket_path) {
         Ok(listener) => listener,
@@ -39,6 +41,12 @@ fn main() {
             exit(1);
         }
     };
+
+    let perm = Permissions::from_mode(0o600);
+    if let Err(e) = std::fs::set_permissions(socket_path, perm) {
+        eprintln!("Could not set permissions {e}");
+        exit(1);
+    }
 
     println!("Listening on {socket_path}...");
     let handler = Arc::new(request_handler::RequestHandler::new(vault));
